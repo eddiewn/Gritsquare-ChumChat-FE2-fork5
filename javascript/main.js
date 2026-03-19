d3a91ff1c15d0155ee69a8dbcad251048eacb2e4
jackgustafsson <134591583+jackgustafsson@users.noreply.github.com>
2026-03-19 15:02:37 +0100

    Added login-system & other firebase-functions

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,63 @@
-import { reference, db } from "./firebase.js"
-import { push, onValue, remove, ref } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
+import { db, messagesRef, repliesRef } from "./firebase.js";
+import { currentUser } from "./auth.js";
+import { push, set, onValue, remove, ref, get } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 import { RenderMessageBox } from "./RenderMessageBox.js";
 
-onValue(reference, snapshot => {
-    const data = snapshot.val();
-    console.log(data);
+export const sendMessage = (text) => {
+    if (!currentUser) return alert("Log in first dumbass!");
+    const newMsg = push(messagesRef);
+
+    const messageData = {
+        message: text,
+        message_id: newMsg.key,
+        user_id: currentUser.user_id,
+        timestamp: Date.now()
+    }
+
+    set(newMsg, messageData);
+}
+
+export const sendReply = (messageId, text) => {
+    const newReply = push(repliesRef);
+
+    const replyData = {
+        message: text,
+        message_id: messageId,
+        user_id: currentUser.user_id,
+        timestamp: Date.now()
+    }
+
+    set(newReply, replyData);
+}
+
+onValue(messagesRef, snapshot => {
+    const messages = snapshot.val();
+    console.log(messages);
+
+    //document.querySelector(".chat-container").innerHTML = "";  <= TA BORT SNEDSTRECKEN EFTER IMPLEMENTERING, ANNARS KOMMER MEDDELANDEN DUPLICERAS!
+    for (let id in messages) {
+        const msg = messages[id];
+        //RenderMessageBox(currentUser, msg);  <= TA BORT SNEDSTRECKEN, DÅ RENDERMESSAGEBOX SKA SENARE ANVÄNDAS HÄR!
+    }
+
 }, error => {
     console.error("Firebase read failed:", error);
-    alert("Kunde inte hämta data från databasen.")
+    alert("Couldn't get messages from database.")
 });
 
+export const deleteMessage = async (messageId) => {
+    await remove(ref(db, "messages/" + messageId));
+
+    const snapshot = await get(repliesRef);
+    const replies = snapshot.val();
+    if (!replies) return;
+
+    for (let id in replies) {
+        if (replies[id].message_id === messageId) {
+            await remove(ref(db, "replies/" + id));
+        }
+    }
+}
+
+//test
 RenderMessageBox("Eddie", "I hate my life");
