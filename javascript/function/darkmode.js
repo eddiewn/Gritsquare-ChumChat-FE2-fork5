b1d94126954387ea26c41af6b090ab29c5dbed17
andi49 <46962871+andi49@users.noreply.github.com>
2026-03-20 11:18:24 +0100

    update darkmode

--- javascript/function/darkmode.js
+++ javascript/function/darkmode.js
@@ -1,6 +1,8 @@
 function darkMode() {
     const darkmodeButton = document.querySelector('#darkmodeButton');
     const main = document.querySelector('#main');
+    const sidenav = document.querySelector('#sidenav')
+    const sidenava = document.querySelector('.sidenav')
 
     if (!darkmodeButton || !main) {
         console.error("Element saknas i DOM");
@@ -9,6 +11,8 @@ function darkMode() {
 
     darkmodeButton.addEventListener('click', () => {
         main.classList.toggle("dark-mode");
+        sidenav.classList.toggle('dark-modenav')
+        sidenava.classList.toggle('dark-modeA')
     });
 }
 
